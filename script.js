482e0ebaaba65a8e8e08ba3e16195c11ad9a6ecb
ZD-ong <384474574@qq.com>
2018-01-17 22:54:07 +0800

    add eraser function

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 var canvas = document.getElementById('main')
 
-var pageWidth = document.documentElement.clientWidth
-var pageHeight = document.documentElement.clientHeight
+fullScreen()
 
-canvas.width = pageWidth
-canvas.height = pageHeight
-
-window.onresize = function () {
+//获取页面宽高
+function fullScreen() {
     var pageWidth = document.documentElement.clientWidth
     var pageHeight = document.documentElement.clientHeight
 
@@ -14,35 +11,50 @@ window.onresize = function () {
     canvas.height = pageHeight
 }
 
+window.onresize = function () {
+    fullScreen()
+}
+
 var context = canvas.getContext('2d')
 
-var painting = false
+var using = false
 var lastPoint = { x: undefined, y: undefined }
 
 //当鼠标按下
 canvas.onmousedown = function (a) {
-    painting = true
+
     var x = a.clientX
     var y = a.clientY
-    lastPoint = { 'x': x, 'y': y }
-    drawCircle(x, y, 2)
+    if (eraserEnable) {
+        using = true
+        context.clearRect(x-5, y-5, 10, 10)
+    } else {
+        using = true
+        lastPoint = { 'x': x, 'y': y }
+    }
+
 }
 
 //鼠标移动
 canvas.onmousemove = function (a) {
-    if (painting) {
-        var x = a.clientX
-        var y = a.clientY
-        var newPoint = { 'x': x, 'y': y }
-        drawCircle(x, y, 2)
-        drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
-        lastPoint = newPoint //时刻更新终点
+    var x = a.clientX
+    var y = a.clientY
+    if (eraserEnable) {
+        if (using) {
+            context.clearRect(x-5, y-5, 10, 10)
+        }
+    } else {
+        if (using) {
+            var newPoint = { 'x': x, 'y': y }
+            drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+            lastPoint = newPoint //时刻更新终点
+        }
     }
 }
 
 //鼠标抬起
 canvas.onmouseup = function (a) {
-    painting = false
+    using = false
 }
 
 
@@ -63,4 +75,10 @@ function drawLine(x1, y1, x2, y2) {
     context.lineTo(x2, y2) //终点
     context.stroke()
     context.closePath()
+}
+
+//function for eraser
+var eraserEnable = false
+eraser.onclick = function () {
+    eraserEnable = !eraserEnable
 }
